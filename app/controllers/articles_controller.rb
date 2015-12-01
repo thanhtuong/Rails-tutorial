c079425159849255a139c4056c93104fb4cd9f60
Thanh Tuong <thanhtuong@kms-technology.com>
2015-12-01 17:26:19 +0700

    Delete comment and authenticate

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: 'thanh', password: '123', except: [:index, :show]
   def index
     @articles = Article.all
   end
